0f99a47376accd4e6a4424a9ff13598f42104277
Kelvin Pires <pireskelvin7@gmail.com>
2022-05-08 23:09:11 -0300

    server

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6abb2d8120acd8",
+    pass: "327bb60ae1380e",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Kelvin Pires <pireskelvin7@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
